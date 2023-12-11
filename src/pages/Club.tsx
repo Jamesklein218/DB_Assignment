6091dc0dbf002304baa4bd00d600e01d82ac6574
Tran Ngoc Dnag Khoa <tndangkhoa218@gmail.com>
2023-12-11 13:07:50 +0700

    feat: add list player in club

--- src/pages/Club.tsx
+++ src/pages/Club.tsx
@@ -1,37 +1,49 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Page from "../layout/Page";
 
 import clubLogo from "../assets/images/clublogo.png";
 import { useEffect, useState } from "react";
 import { axios } from "../custom-axios";
 import { API_URL } from "../config";
-import { Club as TClub } from "../types";
+import { Competitor, Club as TClub } from "../types";
 
 export default function Club() {
   const [club, setClub] = useState<TClub | null>(null);
-  const { season, clubId } = useParams<{ clubId: string, season: string }>();
+  const { season, clubId } = useParams<{ clubId: string; season: string }>();
+  const [competitors, setCompetitors] = useState<Competitor[] | null>(null);
 
   useEffect(() => {
-    axios.get(`${API_URL}club/${clubId}`).then((res) => {
-      console.log(res.data);
-      setClub(res.data.find((club: TClub) => club.Season_year.toString() === season))
-    })
-  }, [])
+    axios
+      .get(`${API_URL}club/${clubId}`)
+      .then((res) => {
+        console.log(res.data);
+        setClub(
+          res.data.find((club: TClub) => club.Season_year.toString() === season)
+        );
+
+        return axios.get(`${API_URL}club/${season}/${clubId}/competitors`);
+      })
+      .then((res) => {
+        console.log(res.data);
+        setCompetitors(res.data);
+      });
+  }, []);
+
+  const navigate = useNavigate();
 
   return (
     <Page>
       <div className="start w-full h-80 object-cover relative">
         <div className="absolute z-10 left-0 bottom-0 flex gap-10">
           <div className="w-80 h-72 bg-white bg-opacity-70 rounded-tr-3xl flex justify-center items-center">
-            <img
-              src={clubLogo}
-              alt="logo"
-            />
+            <img src={clubLogo} alt="logo" />
           </div>
 
           <div className="text-black flex flex-col-reverse py-5">
             <p className="text-xl font-normal">{club?.Website_URL}</p>
-            <p className="md:text-3xl lg:text-4xl xl:text-6xl 2xl:text-8xl font-extrabold">{club?.Name}</p>
+            <p className="md:text-3xl lg:text-4xl xl:text-6xl 2xl:text-8xl font-extrabold">
+              {club?.Name}
+            </p>
           </div>
         </div>
       </div>
@@ -50,9 +62,31 @@ export default function Club() {
                   href={club?.Website_URL}
                   target="_blank"
                   rel="noopener noreferrer"
-                >{club?.Website_URL}</a>
+                >
+                  {club?.Website_URL}
+                </a>
               </td>
             </tr>
+          </tbody>
+        </table>
+
+        <table className="table mt-20">
+          <h1 className="text-3xl font-bold">Competitors</h1>
+          <tbody>
+            {competitors?.map((competitor, index) => (
+              <tr
+                key={competitor.Competitor_id}
+                className="hover:bg-slate-800 hover:text-white hover:cursor-pointer hover:pr-3 border-gray-400 transform duration-300"
+                onClick={() => navigate(`/competitor/${season}/${competitor.Competitor_id}`)}
+              >
+                <th>{index + 1}</th>
+                <td>
+                  {competitor.Last_name} {competitor.First_name}
+                </td>
+                <td>{competitor.Position}</td>
+                <td>{competitor.Nationality}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
--- src/pages/Staff.tsx
+++ src/pages/Staff.tsx
@@ -7,30 +7,17 @@ import { Staff } from "../types";
 
 export default function Staff() {
   const [staff, setStaff] = useState<Staff[] | null>(null); // TODO: replace any with Staff[
-  const [season, setSeason] = useState<number>(2019);
 
   useEffect(() => {
     axios.get(`${API_URL}staff`).then((res) => {
       console.log(res.data)
       setStaff(res.data);
     });
-  }, [season]);
+  }, []);
 
   return (
     <Page>
       <div className="container mx-auto">
-        <select
-          defaultValue={season?.toString()}
-          onChange={(e) => setSeason(parseInt(e.target.value || "1"))}
-          className="select select-bordered w-full max-w-xs mt-20"
-        >
-          <option disabled>Select Season</option>
-          <option>2023</option>
-          <option>2022</option>
-          <option>2021</option>
-          <option>2020</option>
-          <option>2019</option>
-        </select>
 
         <table className="table mt-20">
           <tbody>
@@ -46,9 +33,7 @@ export default function Staff() {
                     {staff.Last_name} {staff.First_name}
                   </td>
                   <td>{staff.Staff_type}</td>
-                  <td className="">
-                    <ArrowRight className="w-5 h-5" />
-                  </td>
+                  
                 </tr>
               ))
             }
